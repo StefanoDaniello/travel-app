f08b54d41f38999b5e69642f82eeca522c3a554f
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-08-03 11:20:35 +0200

    fix

--- src/components/TravelComponents.vue
+++ src/components/TravelComponents.vue
@@ -11,11 +11,11 @@
       </div>
       <div class="form-group">
         <label for="start_date">Start Date:</label>
-        <input type="datetime-local" id="start_date" v-model="form.start_date" required>
+        <input type="date" id="start_date" v-model="form.start_date" required>
       </div>
       <div class="form-group">
         <label for="end_date">End Date:</label>
-        <input type="datetime-local" id="end_date" v-model="form.end_date" required>
+        <input type="date" id="end_date" v-model="form.end_date" required>
       </div>
       <div class="form-group">
         <label for="image">Image:</label>
@@ -33,6 +33,37 @@
         <textarea id="curiosity" v-model="form.curiosity"></textarea>
       </div>
       <div class="form-group">
+        <label for="road_name">Road Name:</label>
+        <input type="text" id="road_name" v-model="form.road_name" required>
+      </div>
+      <div class="form-group">
+        <label for="road_description">Road Description:</label>
+        <textarea id="road_description" v-model="form.road_description"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="road_start_date">Road Start Date:</label>
+        <input type="date" id="road_start_date" v-model="form.road_start_date" required>
+      </div>
+      <div class="form-group">
+        <label for="road_end_date">Road End Date:</label>
+        <input type="date" id="road_end_date" v-model="form.road_end_date" required>
+      </div>
+      <div class="form-group">
+        <label for="road_image">Road Image:</label>
+        <input type="file" id="road_image" @change="handleRoadFileUpload">
+        <div class="image-preview">
+          <img :src="roadPreviewImage" @error="setDefaultRoadImg" alt="Road image preview" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="road_rate">Road Rate:</label>
+        <input type="number" id="road_rate" v-model="form.road_rate" required>
+      </div>
+      <div class="form-group">
+        <label for="road_note">Road Note:</label>
+        <textarea id="road_note" v-model="form.road_note"></textarea>
+      </div>
+      <div class="form-group">
         <button type="submit" class="btn-submit">Add Travel</button>
       </div>
     </form>
@@ -43,72 +74,99 @@
 </template>
 
 <script>
-  import { store } from "../store";
+import { store } from "../store";
 
 export default {
-    name: 'TravelComponent',
-    data() {
-        return {
-            store,
-            form: {
-              name: '',
-              description: '',
-              start_date: '',
-              end_date: '',
-              meal: '',
-              curiosity: ''
-            },
-            imageFile: null,
-            response: null,
-            previewImage: '',
-        }
-    },
-    methods: {
-        handleFileUpload(event) {
-            this.imageFile = event.target.files[0];
-            if (this.imageFile) {
+  name: 'TravelComponent',
+  data() {
+    return {
+      store,
+      form: {
+        name: '',
+        description: '',
+        start_date: '',
+        end_date: '',
+        meal: '',
+        curiosity: '',
+        road_name: '',
+        road_description: '',
+        road_start_date: '',
+        road_end_date: '',
+        road_rate: 0,
+        road_note: ''
+      },
+      imageFile: null,
+      roadImageFile: null,
+      response: null,
+      previewImage: '',
+      roadPreviewImage: '',
+    }
+  },
+  methods: {
+    handleFileUpload(event) {
+      this.imageFile = event.target.files[0];
+      if (this.imageFile) {
         this.previewImage = URL.createObjectURL(this.imageFile);
       }
-        },
-        setDefaultImg(event) {
-            event.target.src = this.store.api.defaultImg;
-        },
-        async submitForm() {
-            const formData = new FormData();
-            formData.append('name', this.form.name);
-            formData.append('description', this.form.description);
-            formData.append('start_date', this.form.start_date);
-            formData.append('end_date', this.form.end_date);
-            formData.append('meal', this.form.meal);
-            formData.append('curiosity', this.form.curiosity);
-            if (this.imageFile) {
-                formData.append('image', this.imageFile);
-            }
+    },
+    handleRoadFileUpload(event) {
+      this.roadImageFile = event.target.files[0];
+      if (this.roadImageFile) {
+        this.roadPreviewImage = URL.createObjectURL(this.roadImageFile);
+      }
+    },
+    setDefaultImg(event) {
+      event.target.src = this.store.api.defaultImg;
+    },
+    setDefaultRoadImg(event) {
+      event.target.src = this.store.api.defaultImg;
+    },
+    async submitForm() {
+      const formData = new FormData();
+      formData.append('name', this.form.name);
+      formData.append('description', this.form.description);
+      formData.append('start_date', this.form.start_date);
+      formData.append('end_date', this.form.end_date);
+      formData.append('meal', this.form.meal);
+      formData.append('curiosity', this.form.curiosity);
+      formData.append('road_name', this.form.road_name);
+      formData.append('road_description', this.form.road_description);
+      formData.append('road_start_date', this.form.road_start_date);
+      formData.append('road_end_date', this.form.road_end_date);
+      formData.append('road_rate', this.form.road_rate);
+      formData.append('road_note', this.form.road_note);
+      if (this.imageFile) {
+        formData.append('image', this.imageFile);
+      }
+      if (this.roadImageFile) {
+        formData.append('road_image', this.roadImageFile);
+      }
 
-            try {
-                const res = await fetch(this.store.api.baseUrl + 'travel', {
-                    method: 'POST',
-                    headers: {
-                      'Accept': 'application/json'
-                    },
-                    body: formData
-                });
-                const data = await res.json();
-                if (res.ok) {
-                    this.response = 'Travel added successfully!';
-                    this.$router.push('/');  // Reindirizzamento alla rotta /
-                } else {
-                    this.response = 'Error adding travel!';
-                }
-                console.log(data);
-            } catch (error) {
-                this.response = 'Error adding travel!';
-                console.error(error);
-            }
+      try {
+        const res = await fetch(this.store.api.baseUrl + 'travel', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json'
+          },
+          body: formData
+        });
+        const data = await res.json();
+        if (res.ok) {
+          this.response = 'Travel added successfully!';
+          this.$router.push('/');  // Reindirizzamento alla rotta /
+        } else {
+          this.response = 'Error adding travel!';
         }
+        console.log(data);
+      } catch (error) {
+        this.response = 'Error adding travel!';
+        console.error(error);
+      }
     }
+  }
 }
 </script>
+
 
 
 

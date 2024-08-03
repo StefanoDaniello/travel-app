56d56c3cc9717879c69f5a64265488ac9a486246
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-08-03 17:09:40 +0200

    fix foto

--- src/components/TravelComponents.vue
+++ src/components/TravelComponents.vue
@@ -32,39 +32,45 @@
         <label for="curiosity">Curiosity:</label>
         <textarea id="curiosity" v-model="form.curiosity"></textarea>
       </div>
-      <div class="form-group">
-        <label for="road_name">Road Name:</label>
-        <input type="text" id="road_name" v-model="form.road_name" required>
-      </div>
-      <div class="form-group">
-        <label for="road_description">Road Description:</label>
-        <textarea id="road_description" v-model="form.road_description"></textarea>
-      </div>
-      <div class="form-group">
-        <label for="road_start_date">Road Start Date:</label>
-        <input type="date" id="road_start_date" v-model="form.road_start_date" required>
-      </div>
-      <div class="form-group">
-        <label for="road_end_date">Road End Date:</label>
-        <input type="date" id="road_end_date" v-model="form.road_end_date" required>
-      </div>
-      <div class="form-group">
-        <label for="road_image">Road Image:</label>
-        <input type="file" id="road_image" @change="handleRoadFileUpload">
-        <div class="image-preview">
-          <img :src="roadPreviewImage" @error="setDefaultRoadImg" alt="Road image preview" />
+
+      <div v-for="(road, index) in form.roads" :key="index" class="road-group">
+        <h3>Road {{ index + 1 }}</h3>
+        <div class="form-group">
+          <label :for="'road_name_' + index">Road Name:</label>
+          <input type="text" :id="'road_name_' + index" v-model="road.name" required>
+        </div>
+        <div class="form-group">
+          <label :for="'road_description_' + index">Road Description:</label>
+          <textarea :id="'road_description_' + index" v-model="road.description"></textarea>
+        </div>
+        <div class="form-group">
+          <label :for="'road_start_date_' + index">Road Start Date:</label>
+          <input type="date" :id="'road_start_date_' + index" v-model="road.start_date" required>
+        </div>
+        <div class="form-group">
+          <label :for="'road_end_date_' + index">Road End Date:</label>
+          <input type="date" :id="'road_end_date_' + index" v-model="road.end_date" required>
+        </div>
+        <div class="form-group">
+          <label :for="'road_image_' + index">Road Image:</label>
+          <input type="file" :id="'road_image_' + index" @change="event => handleRoadFileUpload(event, index)">
+          <div class="image-preview">
+            <img :src="road.previewImage || store.api.defaultImg" @error="setDefaultRoadImg" alt="Road image preview" />
+          </div>
+        </div>
+        <div class="form-group">
+          <label :for="'road_rate_' + index">Road Rate:</label>
+          <input type="number" :id="'road_rate_' + index" v-model="road.rate" required>
+        </div>
+        <div class="form-group">
+          <label :for="'road_note_' + index">Road Note:</label>
+          <textarea :id="'road_note_' + index" v-model="road.note"></textarea>
         </div>
       </div>
+
       <div class="form-group">
-        <label for="road_rate">Road Rate:</label>
-        <input type="number" id="road_rate" v-model="form.road_rate" required>
-      </div>
-      <div class="form-group">
-        <label for="road_note">Road Note:</label>
-        <textarea id="road_note" v-model="form.road_note"></textarea>
-      </div>
-      <div class="form-group">
-        <button type="submit" class="btn-submit">Add Travel</button>
+        <button type="button" class="btn btn-primary mx-3 text-white" @click="addRoad">Add Road</button>
+        <button type="submit" class="btn btn-success text-white">Add Travel</button>
       </div>
     </form>
     <div v-if="response" class="response-message">
@@ -88,18 +94,21 @@ export default {
         end_date: '',
         meal: '',
         curiosity: '',
-        road_name: '',
-        road_description: '',
-        road_start_date: '',
-        road_end_date: '',
-        road_rate: 0,
-        road_note: ''
+        roads: [
+          {
+            name: '',
+            description: '',
+            start_date: '',
+            end_date: '',
+            rate: 0,
+            note: '',
+            previewImage: ''
+          }
+        ]
       },
       imageFile: null,
-      roadImageFile: null,
       response: null,
       previewImage: '',
-      roadPreviewImage: '',
     }
   },
   methods: {
@@ -109,10 +118,11 @@ export default {
         this.previewImage = URL.createObjectURL(this.imageFile);
       }
     },
-    handleRoadFileUpload(event) {
-      this.roadImageFile = event.target.files[0];
-      if (this.roadImageFile) {
-        this.roadPreviewImage = URL.createObjectURL(this.roadImageFile);
+    handleRoadFileUpload(event, index) {
+      const file = event.target.files[0];
+      if (file) {
+        this.form.roads[index].previewImage = URL.createObjectURL(file);
+        this.form.roads[index].imageFile = file;
       }
     },
     setDefaultImg(event) {
@@ -120,6 +130,17 @@ export default {
     },
     setDefaultRoadImg(event) {
       event.target.src = this.store.api.defaultImg;
+    },
+    addRoad() {
+      this.form.roads.push({
+        name: '',
+        description: '',
+        start_date: '',
+        end_date: '',
+        rate: 0,
+        note: '',
+        previewImage: ''
+      });
     },
     async submitForm() {
       const formData = new FormData();
@@ -129,18 +150,21 @@ export default {
       formData.append('end_date', this.form.end_date);
       formData.append('meal', this.form.meal);
       formData.append('curiosity', this.form.curiosity);
-      formData.append('road_name', this.form.road_name);
-      formData.append('road_description', this.form.road_description);
-      formData.append('road_start_date', this.form.road_start_date);
-      formData.append('road_end_date', this.form.road_end_date);
-      formData.append('road_rate', this.form.road_rate);
-      formData.append('road_note', this.form.road_note);
       if (this.imageFile) {
         formData.append('image', this.imageFile);
       }
-      if (this.roadImageFile) {
-        formData.append('road_image', this.roadImageFile);
-      }
+
+      this.form.roads.forEach((road, index) => {
+        formData.append(`roads[${index}][name]`, road.name);
+        formData.append(`roads[${index}][description]`, road.description);
+        formData.append(`roads[${index}][start_date]`, road.start_date);
+        formData.append(`roads[${index}][end_date]`, road.end_date);
+        formData.append(`roads[${index}][rate]`, road.rate);
+        formData.append(`roads[${index}][note]`, road.note);
+        if (road.imageFile) {
+          formData.append(`roads[${index}][image]`, road.imageFile);
+        }
+      });
 
       try {
         const res = await fetch(this.store.api.baseUrl + 'travel', {
@@ -166,9 +190,6 @@ export default {
   }
 }
 </script>
-
-
-
 
 <style lang="scss" scoped>
 .image-preview {

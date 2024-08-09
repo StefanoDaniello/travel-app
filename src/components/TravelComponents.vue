4006150a6c6c4cc2bd9c762172108557c834e5df
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-08-09 18:23:36 +0200

    choose img

--- src/components/TravelComponents.vue
+++ src/components/TravelComponents.vue
@@ -35,7 +35,22 @@
             <label for="image">Immagine viaggio:</label>
             <div>
               <input type="file" id="image" @change="handleFileUpload" class="d-none" />
-              <label for="image" class="btn btn-primary ms-2">Scegli Immagine</label>
+              <label for="image" class="mx-2">
+                <div class="Choose-img" data-tooltip="2Mb Max">
+                  <div class="button-wrapper-Choose-img">
+                    <div class="text">Scegli Immagine</div>
+                    <span class="icon-img-Choose-img">
+                      <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em"
+                        preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17">
+                        </path>
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              </label>
               <div class="image-preview mt-3" v-if="previewImage">
                 <img :src="previewImage || store.api.defaultImg" @error="setDefaultImg" alt="Image preview" />
               </div>
@@ -145,7 +160,22 @@
                 <input type="file" :id="'road_image_' + index" @change="(event) => handleRoadFileUpload(event, index)"
                   class="d-none" />
                 <div>
-                  <label :for="'road_image_' + index" class="btn btn-primary ms-2">Scegli Immagine</label>
+                  <label :for="'road_image_' + index" class="mx-2">
+                    <div class="Choose-img" data-tooltip="2Mb Max">
+                      <div class="button-wrapper-Choose-img">
+                        <div class="text">Scegli Immagine</div>
+                        <span class="icon-img-Choose-img">
+                          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em"
+                            preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17">
+                            </path>
+                          </svg>
+                        </span>
+                      </div>
+                    </div>
+                  </label>
                   <div class="image-preview" v-if="road.previewImage">
                     <img :src="road.previewImage || store.api.defaultImg" @error="setDefaultRoadImg"
                       alt="Road image preview" />
@@ -160,15 +190,17 @@
             </div>
 
             <div class="col-6">
-              <label for="via">Luogo<strong class="text-danger">*</strong>:</label>
-              <input type="text" :id="'road_via_' + index" v-model="road.via" @input="handleRoadInput(index)"
-                class="form-control mb-2" />
-              <div :id="'road_resultsContainer_' + index" class="results-container"></div>
-              <div :id="'road_map_' + index" class="road-map"></div>
+              <div>
+                <label for="via">Luogo<strong class="text-danger">*</strong>:</label>
+                <input type="text" :id="'road_via_' + index" v-model="road.via" @input="handleRoadInput(index)"
+                  class="form-control mb-2" />
+                <div :id="'road_resultsContainer_' + index" class="results-container"></div>
+                <div :id="'road_map_' + index" class="road-map"></div>
+              </div>
               <span v-if="errors[`road_${index}_via`]" class="error-message">{{ errors[`road_${index}_via`]
                 }}</span>
-              <!-- Contenitore per la mappa -->
             </div>
+
 
           </div>
         </div>
@@ -200,9 +232,7 @@
             </div>
             <span>Crea Viaggio</span>
           </button>
-
         </div>
-
       </div>
     </form>
 
@@ -611,6 +641,124 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// Choose-Button-img
+/* Stili per il pulsante */
+.Choose-img {
+  --width: 100%;
+  --height: 50px;
+  --tooltip-height: 35px;
+  --tooltip-width: 90px;
+  --gap-between-tooltip-to-button: 18px;
+  --button-color: #1163ff;
+  --tooltip-color: #c9c9c9ce;
+  width: var(--width);
+  height: var(--height);
+  background: var(--button-color);
+  position: relative;
+  text-align: center;
+  border-radius: 0.45em;
+  font-family: "Arial";
+  transition: background 0.3s;
+}
+
+.Choose-img::before {
+  position: absolute;
+  content: attr(data-tooltip);
+  width: var(--tooltip-width);
+  height: var(--tooltip-height);
+  background-color: var(--tooltip-color);
+  font-size: 0.9rem;
+  color: #111;
+  border-radius: .25em;
+  line-height: var(--tooltip-height);
+  bottom: calc(var(--height) + var(--gap-between-tooltip-to-button) + 10px);
+  left: calc(50% - var(--tooltip-width) / 2);
+}
+
+.Choose-img::after {
+  position: absolute;
+  content: '';
+  width: 0;
+  height: 0;
+  border: 10px solid transparent;
+  border-top-color: var(--tooltip-color);
+  left: calc(50% - 10px);
+  bottom: calc(100% + var(--gap-between-tooltip-to-button) - 10px);
+}
+
+.Choose-img::after,
+.Choose-img::before {
+  opacity: 0;
+  visibility: hidden;
+  transition: all 0.5s;
+}
+
+.text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.button-wrapper-Choose-img,
+.text,
+.icon-img-Choose-img {
+  overflow: hidden;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  color: #fff;
+}
+
+.text {
+  top: 0;
+}
+
+.text,
+.icon-img-Choose-img {
+  transition: top 0.5s;
+}
+
+.icon-img-Choose-img {
+  color: #fff;
+  top: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.icon-img-Choose-img svg {
+  width: 24px;
+  height: 24px;
+}
+
+/* Stili per l'interazione con il pulsante */
+.Choose-img:hover {
+  background: #0149cf;
+}
+
+.Choose-img:hover .text {
+  top: -100%;
+}
+
+.Choose-img:hover .icon-img-Choose-img {
+  top: 0;
+}
+
+.Choose-img:hover::before,
+.Choose-img:hover::after {
+  opacity: 1;
+  visibility: visible;
+}
+
+.Choose-img:hover::after {
+  bottom: calc(var(--height) + var(--gap-between-tooltip-to-button) - 20px);
+}
+
+.Choose-img:hover::before {
+  bottom: calc(var(--height) + var(--gap-between-tooltip-to-button));
+}
+
 // Send Button 
 .send-button {
   font-family: inherit;

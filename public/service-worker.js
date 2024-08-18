bfbab0f85e9e98cd14557c6bea5a1d66feb3a00b
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-08-18 19:58:53 +0200

    add PWA

---
+++ public/service-worker.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/assets/styles/main.scss',
+  '/src/main.js',
+  '/img/icons/vue.js-logo.png',
+];
+
+// Installazione del service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetching e cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+// Attivazione del service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { router } from './router'
 import './assets/styles/main.scss'
 
-import { createApp } from 'vue'
-import { router } from './router'
-import App from './App.vue'
-
-
+// Registrazione del Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(error => {
+        console.error('ServiceWorker registration failed: ', error);
+      });
+  });
+}
 
 createApp(App).use(router).mount('#app')
 

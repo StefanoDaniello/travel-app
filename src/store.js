ec02fc88bdbcf21f77e8d7ab6b00fdbc201d3dca
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-08-20 20:03:29 +0200

    fix

--- src/store.js
+++ src/store.js
@@ -2,8 +2,8 @@ import { reactive } from "vue";
 
 export const store = reactive({
   api: {
-     baseUrl: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/',
-      imgBasePath: import.meta.env.VITE_IMG_BASE_PATH || 'http://127.0.0.1:8000/storage/'
+    baseUrl: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/',
+    imgBasePath: import.meta.env.VITE_IMG_BASE_PATH || 'http://127.0.0.1:8000/storage/',
     defaultImg: "/images/placeholder.png",
   },
   data: {

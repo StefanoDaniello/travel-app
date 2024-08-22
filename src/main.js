292e53c8debd1555212de937cf11c56a7cea20b3
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-08-22 18:08:52 +0200

    remove all pwa

--- src/main.js
+++ src/main.js
@@ -3,17 +3,6 @@ import App from './App.vue'
 import { router } from './router'
 import './assets/styles/main.scss'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch(error => {
-        console.error('ServiceWorker registration failed: ', error);
-      });
-  });
-}
 
 createApp(App).use(router).mount('#app')
 
